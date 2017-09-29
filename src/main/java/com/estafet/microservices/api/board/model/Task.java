003f829182ab8738ebee7f297ad032f52494bafb
dennis williams <dennis.williams@estafet.com>
2017-09-29 22:59:44 +0100

    Bug fixes

--- src/main/java/com/estafet/microservices/api/board/model/Task.java
+++ src/main/java/com/estafet/microservices/api/board/model/Task.java
@@ -1,8 +1,10 @@
 package com.estafet.microservices.api.board.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Task {
 
 	private Integer id;
