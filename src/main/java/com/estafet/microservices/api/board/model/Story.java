60e02cf465decdb4447a06bc337a4b7c51bdd5b4
dennis williams <dennis.williams@estafet.com>
2017-10-01 21:35:37 +0100

    implementation

--- src/main/java/com/estafet/microservices/api/board/model/Story.java
+++ src/main/java/com/estafet/microservices/api/board/model/Story.java
@@ -1,11 +1,14 @@
 package com.estafet.microservices.api.board.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Story {
@@ -28,11 +31,19 @@ public class Story {
 		return projectId;
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "rawtypes" })
 	@JsonIgnore
 	public List<Task> getTasks() {
-		return new RestTemplate().getForObject(System.getenv("TASK_API_SERVICE_URI") + "/story/{storyId}/tasks",
+		List objects =  new RestTemplate().getForObject(System.getenv("TASK_API_SERVICE_URI") + "/story/{storyId}/tasks",
 				List.class, id);
+		List<Task> tasks = new ArrayList<Task>();
+		ObjectMapper mapper = new ObjectMapper();
+		for (Object object : objects) {
+			Task task = mapper.convertValue(object, new TypeReference<Task>() {
+			});
+			tasks.add(task);
+		}
+		return tasks;
 	}
 
 }
