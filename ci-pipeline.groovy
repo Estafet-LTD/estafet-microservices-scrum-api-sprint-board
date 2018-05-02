feb585d3f4b33ec309fc9a996c8b16f6faec6821
Dennis Williams <dennis.williams@estafet.com>
2018-05-02 12:45:21 +0100

    updated automation tasks and new release pipeline

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -9,19 +9,15 @@ node("maven") {
 
 	stage("build and execute unit tests") {
 		sh "mvn clean test"
-		post {
-			always {
-				junit "**/target/surefire-reports/*.xml"
-			}
-		}
+		junit "**/target/surefire-reports/*.xml"
 	}
 
 	stage("build & deploy container") {
-		openshiftBuild namespace: ${project}, buildConfig: ${microservice}, showBuildLogs: "true",  waitTime: "3000000"
+		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "3000000"
 	}
   	  
 	stage("verify container deployment") {
-		openshiftVerifyDeployment namespace: ${project}, depCfg: ${microservice}, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"	
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"	
 	}
 
 	stage("execute the container tests") {
@@ -29,11 +25,7 @@ node("maven") {
 			[ "SPRINT_BOARD_API_SERVICE_URI=http://${microservice}.${project}.svc:8080" ]) {
 			sh "mvn verify -P integration-test"
 		}
-		post {
-			always {
-				junit "**/target/failsafe-reports/*.xml"
-			}
-		}
+		junit "**/target/failsafe-reports/*.xml"
 	}
 
 }
---
+++ release-pipeline.groovy
@@ -0,0 +1,8 @@
+node() {
+
+	stage("checkout") {
+		
+	}
+
+}
+
