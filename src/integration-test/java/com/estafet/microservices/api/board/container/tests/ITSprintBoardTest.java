c77cb58712ca9a94d96ab66a0db5a6799819270c
Dennis Williams <dennis.williams@estafet.com>
2018-05-11 02:09:32 +0100

    fixed tests

--- src/integration-test/java/com/estafet/microservices/api/board/container/tests/ITSprintBoardTest.java
+++ src/integration-test/java/com/estafet/microservices/api/board/container/tests/ITSprintBoardTest.java
@@ -18,18 +18,22 @@ public class ITSprintBoardTest {
 	@Test
 	public void testGetAPI() {
 		get("/api").then()
-			.body("id", is(1));
+			.body("sprint.id", is(1))
+			.body("sprint.startDate", is("2017-10-16 00:00:00"))
+			.body("sprint.endDate", nullValue());
 	}
 
 	@Test
 	public void testGetSprintBoard() {
-		get("sprint/1/board").then()
+		get("/sprint/1/board").then()
 			.body("sprint.id", is(1))
 			.body("sprint.startDate", is("2017-10-16 00:00:00"))
 			.body("sprint.endDate", is("2017-10-20 00:00:00"))
 			.body("sprint.number", is(1))
 			.body("sprint.projectId", is(2))
-			.body("todo.id", hasItems(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12));
+			.body("todo.id", hasItems(1, 2, 7, 8, 9, 11))
+			.body("inProgress.id", hasItems(3, 5, 6, 12))
+			.body("completed.id", hasItems(4, 10));
 	}
 
 
