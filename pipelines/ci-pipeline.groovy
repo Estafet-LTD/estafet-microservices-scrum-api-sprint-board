6db824bfe7b9c1127b7961eb1fce64ab90f8c059
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-04-29 22:18:37 +0100

    build pipe line no longer uses rsync to upload files to the wiremock shared volume. Instead test mocks are sent via the restful interface

--- pipelines/ci-pipeline.groovy
+++ pipelines/ci-pipeline.groovy
@@ -8,19 +8,17 @@ node("maven") {
 	}
 
 	stage("update wiremock") {
-		sh "oc get pods --selector app=wiremock-docker -o json -n ${project} > pods.json"
-		def json = readFile('pods.json');
-		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name
-		sh "oc exec ${pod} -n ${project} -- /bin/sh -i -c \"rm -f /home/wiremock/mappings/*.json\""
-		sh "oc rsync src/integration-test/resources/ ${pod}:/home/wiremock/mappings -n ${project}"
-		openshiftDeploy namespace: project, depCfg: "wiremock-docker", showBuildLogs: "true",  waitTime: "3000000"
-		openshiftVerifyDeployment namespace: project, depCfg: "wiremock-docker", replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
+		def files = findFiles(glob: 'src/integration-test/resources/*.json')
+		files.each { file -> 
+			def json = readFile(file.path)
+			def response = httpRequest url: "http://wiremock-docker.${project}.svc:8080/__admin/mappings/new", httpMode: "POST", validResponseCodes: "200", requestBody: json
+		}
 	}
 
 	stage("build & deploy container") {
 		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "3000000"
 		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
-		sleep time:90
+		sleep time:120
 	}
 
 	stage("container tests") {
