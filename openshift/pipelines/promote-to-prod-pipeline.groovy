40d9e8745e8a4290b5306f3a9dce34401a189dd5
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-09 16:02:52 +0100

    in prod the dc names are prefixed

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -93,7 +93,7 @@ node {
 	
 	stage("flag this microservice as untested") {
 		println "The tests passed successfully"
-		sh "oc patch dc/${microservice} -p '{\"metadata\":{\"labels\":{\"testStatus\":\"untested\"}}}' -n ${project}"		
+		sh "oc patch dc/${env}${microservice} -p '{\"metadata\":{\"labels\":{\"testStatus\":\"untested\"}}}' -n ${project}"		
 	}	
 
 }
