f0b837dbd55c7c08d28a202e224f765daa01c6f1
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-11-06 08:20:07 +0000

    modified pipeline for microservice test environment

--- openshift/pipelines/microservice-pipeline.groovy
+++ openshift/pipelines/microservice-pipeline.groovy
@@ -1,3 +1,28 @@
+@NonCPS
+def getVersions(json) {
+	def tags = new groovy.json.JsonSlurper().parseText(json).status.tags
+	def versions = []
+	for (int i = 0; i < tags.size(); i++) {
+		versions << tags[i]['tag']
+	}
+	return versions
+}
+
+def recentVersion(versions) {
+	def size = versions.size()
+	return versions[size-1]
+}
+
+def getLatestVersion(microservice) {
+	sh "oc get is ${microservice} -o json -n cicd > image.json"
+	def image = readFile('image.json')
+	def versions = getVersions(image)
+	if (versions.size() == 0) {
+		error("There are no images for ${microservice}")
+	}
+	return recentVersion(versions)
+}
+
 node("maven") {
 
 	properties([
@@ -10,28 +35,25 @@ node("maven") {
 	def microservice = "sprint-board"	
 	def version
 
+	stage("get the latest version") {
+		version = getLatestVersion microservice
+	}
+
 	stage("checkout") {
-		version = sh(returnStdout: true, script: "git tag --sort version:refname | tail -1").trim()
-		git branch: version, url: "https://github.com/${params.GITHUB}/estafet-microservices-scrum-api-sprint-board"
-	}
-
-	stage("create build config") {
-			sh "oc process -n ${project} -f openshift/templates/${microservice}-build-config.yml -p NAMESPACE=${project} -p GITHUB=${params.GITHUB} -p DOCKER_IMAGE_LABEL=${version} SOURCE_REPOSITORY_REF=${version} | oc apply -f -"
-	}
-
-	stage("execute build") {
-		openshiftBuild namespace: project, buildConfig: microservice, waitTime: "300000"
-		openshiftVerifyBuild namespace: project, buildConfig: microservice, waitTime: "300000" 
+		checkout scm: [$class: 'GitSCM', 
+      userRemoteConfigs: [[url: "https://github.com/${params.GITHUB}/estafet-microservices-scrum-api-sprint-board"]], 
+      branches: [[name: "refs/tags/${version}"]]], changelog: false, poll: false		
 	}
 
 	stage("create deployment config") {
-		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p DOCKER_NAMESPACE=${project} -p DOCKER_IMAGE_LABEL=${version} | oc apply -f -"
+		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p ENV=${env} -p DOCKER_NAMESPACE=${project} -p DOCKER_IMAGE_LABEL=${version} | oc apply -f -"
+		sh "oc set env dc/${env}${microservice} SPRINT_API_SERVICE_URI=http://${env}sprint-api.${project}.svc:8080 STORY_API_SERVICE_URI=http://${env}story-api.${project}.svc:8080 TASK_API_SERVICE_URI=http://${env}task-api.${project}.svc:8080 JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"
 	}
-
+	
 	stage("execute deployment") {
-		openshiftDeploy namespace: project, depCfg: microservice,  waitTime: "3000000"
-		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000" 
+		openshiftDeploy namespace: project, depCfg: "${env}${microservice}",  waitTime: "3000000"
+		openshiftVerifyDeployment namespace: project, depCfg: "${env}${microservice}", replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000" 
 	}
-
+	
 }
 
